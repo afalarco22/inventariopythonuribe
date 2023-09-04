6b64f336b3eacfeafff870d274bc655d02cc0f6c
afalarco22 <afalarco@unal.edu.co>
2023-09-03 20:20:33 -0500

    feat: se agrega metodo de buscar, editar y reemplazar

--- tiendaslaganga.py
+++ tiendaslaganga.py
@@ -1,7 +1,7 @@
 # menu d opciones
 # 1. Ingresar un producto en la bodega
 # 2: verificar productos en bodega
-# 3: buscar un porducto en la bodega
+# 3: buscar un producto en la bodega
 # 4: editar un producto en la bodega
 # 5. retirar producto de la bodega
 # 6: numero 6 salir
@@ -11,18 +11,42 @@
 opc = 0
 print("TIENDA EL GANGAZO")
 print("***********************************")
-print("Ingresar un producto en la bodega")
-print("Verificar productos en bodega")
-print("Buscar un porducto en la bodega")
-print("Editar un producto en la bodega")
-print("Retirar producto de la bodega")
-print("Numero 6 salir")
+print("1. Ingresar un producto en la bodega")
+print("2. Verificar productos en bodega")
+print("3. Buscar un producto en la bodega")
+print("4. Editar un producto en la bodega")
+print("5. Retirar producto de la bodega")
+print("6. Numero 6 salir")
 print("***********************************")
 
-productos = []
+productos = [
+    {
+        "nombre": "Camisa",
+        "codigo": 123,
+        "descripcion": "Camisa tipo polo",
+        "foto": "www.yahoo.es",
+        "precio": 145000,
+        "stock": 40,
+        "fechaEntradaBodega": "12-12-2022"
+    },
+
+    {
+        "nombre": "pantalon",
+        "codigo": 456,
+        "descripcion": "pantalon clásico",
+        "foto": "www.yahoo.es",
+        "precio": 250000,
+        "stock": 23,
+        "fechaEntradaBodega": "12-11-2022"
+    }
+
+
+
+]
 
 while opc != 6:
     producto = {}
+    print("Ha entrado en la opción 1")
     opc = int(input("Digita la opción deseada: "))
     if opc == 1:
         producto["nombre"] = input("digite el nombre del producto: ")
@@ -31,24 +55,62 @@ while opc != 6:
         producto["foto"] = input("digite la URL de la foto: ")
         producto["precio"] = float(input("digite el precio del producto. "))
         producto["stock"] = int(input("digite el stock del producto: "))
-        producto["fechaEntradaBodega"] = input("digite la fecha de entrada ")   
+        producto["fechaEntradaBodega"] = input("digite la fecha de entrada ")
+        productos.append(producto)
+    
+
     elif opc == 2:
+        print("Ha entrado en la opción 2")
         for productoSeleccionado in productos:
-            print(f"codigo = {productoSeleccionado['codigo']} ")
+            print(f'codigo = {productoSeleccionado["codigo"]} ')
             print(f"nombre = {productoSeleccionado['nombre']} ")
             print(f"descripcion = {productoSeleccionado['descripcion']} ")
             print(f"Cantidad en bodega  = {productoSeleccionado['stock']} ")
-            print(f"fecha de entrada en bodega = {productoSeleccionado['fechaEntradaBodega']} \n ")
+            print(f"fecha de entrada en bodega = {productoSeleccionado['fechaEntradaBodega']}")
             print(f"Precio = {productoSeleccionado['precio']} ")
+
+
     elif opc == 3:
-        pass
+        cod = int(input("Ingrese el código del producto que está buscando "))  
+
+        for productoSeleccionado in productos:
+            if cod == productoSeleccionado["codigo"]:
+                print(f'codigo = {productoSeleccionado["codigo"]}')
+                print(f"nombre = {productoSeleccionado['nombre']} ")
+                print(f"descripcion = {productoSeleccionado['descripcion']} ")
+                print(f"Cantidad en bodega  = {productoSeleccionado['stock']} ")
+                print(f"fecha de entrada en bodega = {productoSeleccionado['fechaEntradaBodega']} ")
+                print(f"Precio = {productoSeleccionado['precio']} ")
+            else: pass
+            
+            
     elif opc == 4:
-        pass
+        cod = int(input("Ingrese el código del producto que quiere editar: "))
+        for indice,producto in enumerate(productos):
+            if cod == productos[indice]["codigo"]:
+                productos[indice]["nombre"] = input("digite el nombre del producto: ")
+                productos[indice]["codigo"] =  int(input("digite el codigo del producto: "))
+                productos[indice]["descripcion"] = input("digite la descripción producto: ")
+                productos[indice]["foto"] = input("digite la URL de la foto del prodcuto: ")
+                productos[indice]["precio"] = float(input("digite el precio del producto: "))
+                productos[indice]["stock"] = int(input("digite el stock del producto: "))
+                productos[indice]["fechaEntradaBodega"] = input("digite fecha de llegada del producto: ")
+            else: pass
+       
     elif opc == 5 :
-        pass
+
+        cod = int(input("Ingrese el código del producto que quiere eliminar: "))  
+        for productoSeleccionado in productos:
+            if cod == productoSeleccionado["codigo"]:
+                productos.remove(productoSeleccionado)   
+            else: pass
+        
     elif opc == 6 :
         pass
     else:
         print("opción invalida, vuelva a intentarlo")
+
+print("ha salido del programa")
+
 
 
